5feabe0a39bdd3296a6a1e4683927657ae736260
wunknown2 <wunknown2@gmail.com>
2025-12-25 17:12:22 +0900

    last commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="icon">
+        {" "}
+        <img src="./src/assets/logo.svg" alt="Logo" />
+      </div>
+      <div className="buttons">
+        <a className="first">About us</a>{" "}
+        <button className="second">les`t Talk</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+const Main = () => {
+  return (
+    <main className="desktop">
+        <div className="div">   
+            <h2>We build dreams, not just walls</h2>
+            <p> 
+                We are a team of passionate designers and developers who create 
+                beautiful, functional websites and applications.
+            </p>
+               
+            <button className="button">let`s Talk </button>
+        </div>
+    </main>
+  );
+}
+
+export default Main;
